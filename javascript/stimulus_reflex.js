529da01b931ed54d31cd2d89350f5ba4bab5d47b
Lucius Choi <lucius.choi@gmail.com>
2021-12-26 06:22:49 +0900

    updated stimulus package

--- javascript/stimulus_reflex.js
+++ javascript/stimulus_reflex.js
@@ -1,4 +1,4 @@
-import { Controller } from 'stimulus'
+import { Controller } from '@hotwired/stimulus'
 import { dispatchLifecycleEvent } from './lifecycle'
 import { uuidv4, serializeForm } from './utils'
 import { beforeDOMUpdate, afterDOMUpdate, routeReflexEvent } from './callbacks'
